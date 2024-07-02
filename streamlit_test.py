0ed435b3fab02e99036f0c2c098156346156fec7
Cole Spencer <80274122+colespencer1453@users.noreply.github.com>
2024-07-02 15:31:49 -0400

    wip

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import fitz  # PyMuPDF
+import pymupdf  # PyMuPDF
 import pandas as pd
 import re
 import openpyxl
@@ -9,7 +9,7 @@ import base64
 # Function to extract text from PDF using PyMuPDF
 def extract_text_from_pdf(file):
     text = ""
-    with fitz.open(stream=file.read(), filetype="pdf") as pdf:
+    with pymupdf.open(stream=file.read(), filetype="pdf") as pdf:
         for page in pdf:
             text += page.get_text()
     return text
